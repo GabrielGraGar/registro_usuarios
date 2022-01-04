503ad44be90f14bbbc1487d4f7248e43624b7253
Gabriel Granados <gg18018@ues.edu.sv>
2022-01-04 15:34:51 -0600

    Se ha agregado metodo switch a la api para que identifique el t´tipo de peticion del usuario

--- backend/api/usuarios.php
+++ backend/api/usuarios.php
@@ -1,7 +1,32 @@
 <?php>
 
-//localhost: 8080/.../api/usuarios.php
+    //localhost: 8080/.../api/usuarios.php
 
-//recibir peticiones del usuario
+    //recibir peticiones del usuario
+    switch($_SERVER['REQUEST_METHOD']){
+
+        case 'POST':
+            echo 'Guardar';
+        break;
+        case 'GET':
+            echo 'Obtener';
+        break;
+        case 'PUT':
+            echo 'Actualizar un usuario';
+        break;
+        case 'DELETE':
+            echo 'Eliminar un usuario';
+        break;
+
+    }
+    //crear
+
+    //obtener un usuario
+
+    //Obtener todos los usuarios
+
+    //actualizar un usuario
+
+    //Eliminar un usuario
 
 ?>
