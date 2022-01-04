02a4fbbec861d4ca1a2e5dab963e1fe9a3ebf13d
Gabriel Granados <gg18018@ues.edu.sv>
2022-01-04 16:16:13 -0600

    Se ha creado la estructura para recibir las peticiones en el api de la aplicacion

--- backend/api/usuarios.php
+++ backend/api/usuarios.php
@@ -1,32 +1,40 @@
-<?php>
+<?php
 
     //localhost: 8080/.../api/usuarios.php
+    header("Content-Type: application/json")
 
     //recibir peticiones del usuario
     switch($_SERVER['REQUEST_METHOD']){
 
         case 'POST':
-            echo 'Guardar';
+            $_POST= json_decode(file_get_contents('php://imput'),true);
+            $resultado['mensaje']= "Guardar informacion:". json_encode($_POST);
+            echo json_encode($resultado);
         break;
+
         case 'GET':
-            echo 'Obtener';
+            if(isset($_GET['id'])){
+                $resultado['mensaje']= 'Retornar el usuario con el id: '. $_GET['id'];
+                echo json_encode($resultado);
+            }
+            else{
+                $resultado['mensaje']= 'Retornar todos los usuarios'; 
+                echo json_encode($resultado);
+            }
+                  
         break;
+
         case 'PUT':
-            echo 'Actualizar un usuario';
-        break;
+            $_PUT= json_decode(file_get_contents('php://imput'),true);
+            $resultado['mensaje']= 'Actualizar el usuario con el id: '. $_GET['id'] . ', informacion a actualizar: '. json_encode($_PUT);
+            echo json_encode($resultado);
+            break;
+
         case 'DELETE':
-            echo 'Eliminar un usuario';
+            $resultado['mensaje']= 'Eliminar un usuario con el id: '. $_GET['id'];
+            echo json_encode($resultado);
         break;
 
     }
-    //crear
-
-    //obtener un usuario
-
-    //Obtener todos los usuarios
-
-    //actualizar un usuario
-
-    //Eliminar un usuario
 
 ?>
